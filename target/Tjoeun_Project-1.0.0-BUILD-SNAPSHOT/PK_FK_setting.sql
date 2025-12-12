529f04db3b5158571bd28e455022b109a34213db
학생 이하영 <sjo6382@knou.ac.kr>
2025-12-12 16:01:48 +0900

    login controller 47 error check

---
+++ target/Tjoeun_Project-1.0.0-BUILD-SNAPSHOT/PK_FK_setting.sql
@@ -0,0 +1,64 @@
+alter table member
+add constraint pk_cnum PRIMARY KEY(cnum);
+
+alter table fboard
+add constraint pk_fboard_idx PRIMARY KEY(idx);
+
+alter table board
+add constraint pk_board_idx PRIMARY KEY(idx);
+
+alter table schedule
+add constraint pk_schedule_idx primary key(schedule_idx);
+
+alter table meal
+add(cnum number(*));
+
+alter table meetingroom
+add(cnum number(*));
+
+
+
+ALTER TABLE atten_date
+ADD CONSTRAINT fk_cnum_member_to_atten foreign KEY(cnum) references member (cnum);
+
+ALTER TABLE elecappr
+ADD CONSTRAINT fk_cnum_member_to_elec foreign KEY(cnum) references member (cnum);
+
+ALTER TABLE board
+ADD CONSTRAINT fk_cnum_member_to_board foreign KEY(cnum) references member (cnum);
+
+ALTER TABLE fboard
+alter table member
+add constraint pk_cnum PRIMARY KEY(cnum);
+
+alter table fboard
+add constraint pk_fboard_idx PRIMARY KEY(idx);
+
+alter table board
+add constraint pk_board_idx PRIMARY KEY(idx);
+
+
+
+ALTER TABLE atten_date
+ADD CONSTRAINT fk_cnum_member_to_atten foreign KEY(cnum) references member (cnum);
+
+ALTER TABLE elecappr
+ADD CONSTRAINT fk_cnum_member_to_elec foreign KEY(cnum) references member (cnum);
+
+ALTER TABLE board
+ADD CONSTRAINT fk_cnum_member_to_board foreign KEY(cnum) references member (cnum);
+
+ALTER TABLE fboard
+ADD CONSTRAINT fk_cnum_member_to_fboard foreign KEY(cnum) references member (cnum);
+
+alter table meal
+ADD CONSTRAINT fk_cnum_member_to_meal foreign KEY(cnum) references member (cnum);
+
+alter table meetingroom
+ADD CONSTRAINT fk_cnum_member_to_meetingroom foreign KEY(cnum) references member (cnum);
+
+
+commit;
+
+select * from meal;
+select * from meetingroom;
